1d1f5f7fe832496cf56170504d7fb7a00b60cc36
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-09-21 11:18:41 -0400

    Experimenting with layers

---
+++ experiments.py
@@ -0,0 +1,19 @@
+INPUTS = 9 * 6 * 6
+OUTPUTS = 18
+
+
+LAYER_COUNT = 5
+
+RANGE = INPUTS - OUTPUTS
+
+print(RANGE)
+
+DIFF = RANGE // LAYER_COUNT
+print(DIFF)
+
+
+LAYERS = []
+
+for i in range(1,LAYER_COUNT):
+  LAYERS.append(INPUTS - DIFF * i)
+print(LAYERS)
