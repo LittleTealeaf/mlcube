e9bfcd3c79c7891fe569d12686302b314c00115b
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-10-31 10:05:40 -0400

    Updated agent layer sizes

--- script.py
+++ script.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
             data_spec, batch_size=BATCH_SIZE, max_length=MAX_BUFFER_LENGTH
         )
 
-        agent = Agent([700, 600, 500, 343, 200], f"agents/{branch}")
+        agent = Agent([600, 600, 600, 600, 600], f"agents/{branch}")
 
         # Pre-fill the replay data
         if PREFILL_DATA:
