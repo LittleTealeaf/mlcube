38b6797db0927b20e33ff089be4dfe5efdf90c5e
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-10-31 10:05:55 -0400

    Reduce target interval to 250

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ def exponential_decay(initial, index, decay_rate, decay_interval=1):
 
 EVALUATE_INTERVAL = 10
 SAVE_INTERVAL = 10
-TARGET_INTERVAL = 500
+TARGET_INTERVAL = 250
 GAMMA=0.99
 
 BATCH_SIZE = 1000
