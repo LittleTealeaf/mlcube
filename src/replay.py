bc188518b6126f0c1851af8c447b2aea01048371
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-01-06 17:31:33 -0500

    Clear up code

--- src/replay.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-def create_replay_buffer():
-  data_spec = (
-    tf.TensorSpec([9*6*6],dtype=tf.float32),
-    tf.TensorSpec([1],dtype=tf.int32),
-    tf.TensorSpec([9*6*6],dtype=tf.float32)
-  )
