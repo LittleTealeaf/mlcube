0a31ffdcd7bc0389d8662db124b429d2b5eec860
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-01-08 15:43:30 -0500

    FINALLY got apply_move to work

--- src/environment.rs
+++ src/environment.rs
@@ -74,6 +74,30 @@ impl Face {
             _ => Err("Index Out of Bounds"),
         }
     }
+
+    fn get_perms(&self) -> [[usize; 4]; 5] {
+        match self {
+            Face::R => PERMS_R,
+            Face::U => PERMS_U,
+            Face::F => PERMS_F,
+            Face::L => PERMS_L,
+            Face::D => PERMS_D,
+            Face::B => PERMS_B,
+        }
+    }
+}
+
+impl ToString for Face {
+    fn to_string(&self) -> String {
+        match self {
+            Face::R => String::from("R"),
+            Face::U => String::from("U"),
+            Face::F => String::from("F"),
+            Face::L => String::from("L"),
+            Face::D => String::from("D"),
+            Face::B => String::from("B"),
+        }
+    }
 }
 
 impl PartialEq for Face {
@@ -113,6 +137,28 @@ pub enum Move {
     Two(Face),
 }
 
+impl Move {
+    fn from_index(index: isize) -> Result<Move, &'static str> {
+        let face = Face::from_index(index as usize / 3)?;
+        match index % 3 {
+            0 => Ok(Move::Normal(face)),
+            1 => Ok(Move::Prime(face)),
+            2 => Ok(Move::Two(face)),
+            _ => Err("How did you manage to get here?!"),
+        }
+    }
+}
+
+impl ToString for Move {
+    fn to_string(&self) -> String {
+        match self {
+            Move::Normal(f) => format!("{}", f.to_string()),
+            Move::Prime(f) => format!("{}'", f.to_string()),
+            Move::Two(f) => format!("{}2", f.to_string()),
+        }
+    }
+}
+
 impl PartialEq for Move {
     fn eq(&self, other: &Self) -> bool {
         match self {
@@ -147,18 +193,78 @@ impl Cube {
 
         Cube { state }
     }
+
+    fn is_solved(&self) -> bool {
+        for i in 0..6 {
+            let face = Face::from_index(i).unwrap();
+            for j in 0..9 {
+                if self.state[i * 9 + j].ne(&face) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    fn apply_move(&mut self, action: Move) {
+        match action {
+            Move::Normal(face) => {
+                for row in face.get_perms()  {
+                    let aside = self.state[row[row.len() - 1]];
+                    let len = row.len();
+                    for i in 0..(len - 1) {
+                        self.state[row[len - i - 1]] = self.state[row[len - i - 2]];
+                    }
+                    self.state[row[0]] = aside;
+                }
+            }
+            Move::Prime(face) => {
+                for row in face.get_perms() {
+                    let aside = self.state[row[0]];
+                    for i in 0..(row.len() - 1) {
+                        self.state[row[i]] = self.state[row[i+1]];
+                    }
+                    self.state[row[row.len() - 1]] = aside;
+                }
+            }
+            Move::Two(face) => {
+                for row in face.get_perms() {
+                    for i in [0,1] {
+                        let aside = self.state[row[i]];
+                        self.state[row[i]] = self.state[row[i + 2]];
+                        self.state[row[i + 2]] = aside;
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn get_all_moves() -> Vec<Move> {
+        let mut moves: Vec<Move> = Vec::new();
+
+        for face in [Face::R, Face::U, Face::F, Face::L, Face::B, Face::D] {
+            moves.push(Move::Normal(face));
+            moves.push(Move::Prime(face));
+            moves.push(Move::Two(face));
+        }
+
+        moves
+    }
+
     mod enum_face {
         use super::*;
 
         #[test]
         fn same_faces_are_equal() {
-            assert!(Face::R.eq(&Face::R), "The same face should be equal to itself");
+            assert!(
+                Face::R.eq(&Face::R),
+                "The same face should be equal to itself"
+            );
         }
 
         #[test]
@@ -180,12 +286,30 @@ mod tests {
             for i in 0..6 {
                 values.push(Face::from_index(i).unwrap());
             }
-            assert!(values.contains(&Face::R), "R should be accessible using the from_index method");
-            assert!(values.contains(&Face::U), "U should be accessible using the from_index method");
-            assert!(values.contains(&Face::F), "F should be accessible using the from_index method");
-            assert!(values.contains(&Face::L), "L should be accessible using the from_index method");
-            assert!(values.contains(&Face::D), "D should be accessible using the from_index method");
-            assert!(values.contains(&Face::B), "B should be accessible using the from_index method");
+            assert!(
+                values.contains(&Face::R),
+                "R should be accessible using the from_index method"
+            );
+            assert!(
+                values.contains(&Face::U),
+                "U should be accessible using the from_index method"
+            );
+            assert!(
+                values.contains(&Face::F),
+                "F should be accessible using the from_index method"
+            );
+            assert!(
+                values.contains(&Face::L),
+                "L should be accessible using the from_index method"
+            );
+            assert!(
+                values.contains(&Face::D),
+                "D should be accessible using the from_index method"
+            );
+            assert!(
+                values.contains(&Face::B),
+                "B should be accessible using the from_index method"
+            );
         }
     }
 
@@ -194,14 +318,26 @@ mod tests {
 
         #[test]
         fn same_moves_are_equal() {
-            assert!(Move::Normal(Face::R).eq(&Move::Normal(Face::R)), "Moves of the same type and face should be equal");
+            assert!(
+                Move::Normal(Face::R).eq(&Move::Normal(Face::R)),
+                "Moves of the same type and face should be equal"
+            );
         }
 
         #[test]
         fn different_moves_are_not_equal() {
-            assert!(!Move::Prime(Face::U).eq(&Move::Two(Face::U)), "Moves of different types but the same face should not be equal");
-            assert!(!Move::Normal(Face::F).eq(&Move::Prime(Face::L)), "Moves of different types and different faces should not be equal");
-            assert!(!Move::Two(Face::D).eq(&Move::Two(Face::B)), "Moves of the same type but different faces should not be equal");
+            assert!(
+                !Move::Prime(Face::U).eq(&Move::Two(Face::U)),
+                "Moves of different types but the same face should not be equal"
+            );
+            assert!(
+                !Move::Normal(Face::F).eq(&Move::Prime(Face::L)),
+                "Moves of different types and different faces should not be equal"
+            );
+            assert!(
+                !Move::Two(Face::D).eq(&Move::Two(Face::B)),
+                "Moves of the same type but different faces should not be equal"
+            );
         }
     }
 
@@ -220,7 +356,7 @@ mod tests {
                     Face::F => 2,
                     Face::D => 3,
                     Face::L => 4,
-                    Face::B => 5
+                    Face::B => 5,
                 }] += 1;
             }
 
@@ -230,6 +366,42 @@ mod tests {
             assert_eq!(counts[3], 9, "Frequency of Bottom Faces should be 9");
             assert_eq!(counts[4], 9, "Frequency of Left Faces should be 9");
             assert_eq!(counts[5], 9, "Frequency of Back Faces should be 9");
+        }
+
+        #[test]
+        fn new_cube_is_solved() {
+            let cube = Cube::new();
+
+            assert!(cube.is_solved());
+        }
+
+        #[test]
+        fn applying_move_changes_state() {
+            for action in get_all_moves() {
+                let mut cube = Cube::new();
+                let string = action.to_string();
+                cube.apply_move(action);
+                assert!(
+                    !cube.is_solved(),
+                    "Applying {} to a solved cube should make it unsolved",
+                    string
+                );
+            }
+        }
+
+        #[test]
+        fn applying_repeated_move_should_return_to_solved() {
+            for i in 0..18 {
+                let mut cube = Cube::new();
+                for _ in 0..4 {
+                    cube.apply_move(Move::from_index(i).unwrap());
+                }
+                assert!(
+                    cube.is_solved(),
+                    "Applying {} four times should return a solved cube to a solved state",
+                    Move::from_index(i).unwrap().to_string()
+                );
+            }
         }
     }
 }
