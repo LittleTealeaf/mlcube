7b4cc306dd3ec01345b9fe2e73e9163d9d0bda96
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-02-15 14:13:58 -0500

    started work on the environment class

---
+++ model/src/environment.py
@@ -0,0 +1,22 @@
+import numpy as np
+import tensorflow as tf
+
+
+
+class Environment:
+  def __init__(self):
+    ...
+
+
+# # %%
+
+# This means that we can use array slicing with slicing arrays to automate things! :O
+# import numpy as np
+
+# arr = np.array([1,2,3,4])
+# print(arr)
+# arr[[2,3]] = arr[[3,2]]
+# print(arr)
+
+# # %%
+
