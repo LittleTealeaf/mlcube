f5fab11174751b0d1146d4c82c81c357187fd116
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-02-19 13:41:07 -0500

    Loaded prior data into database

--- sql/initialize.sql
+++ sql/initialize.sql
@@ -1,9 +1,24 @@
 USE mlcube;
-CREATE TABLE test (
-  id INTEGER PRIMARY KEY,
-  value INTEGER
-);
 
+CREATE TABLE Models
+(
+  Modelid INT NOT NULL IDENTITY PRIMARY KEY,
+  ModelName VARCHAR(100) NOT NULL,
+)
 
-USE mlcube;
-SELECT * FROM test
+CREATE TABLE Nodes
+(
+  Weight FLOAT(53),
+  Bias FLOAT(53),
+  Layer INT NOT NULL,
+  Node INT NOT NULL,
+  Modelid INT FOREIGN KEY REFERENCES Models(Modelid)
+)
+
+CREATE TABLE Epochs
+(
+  Modelid INT FOREIGN KEY REFERENCES Models(Modelid),
+  Epoch INT NOT NULL,
+  Loss FLOAT(53),
+  Reward FLOAT(53)
+)
