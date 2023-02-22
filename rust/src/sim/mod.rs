32905ea19fea57332e38358d95082d2cb10481e9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-02-22 13:13:47 -0500

    start from scratch!

--- rust/src/sim/mod.rs
+++
@@ -1,8 +0,0 @@
-mod face;
-pub use face::*;
-mod rotation;
-pub use rotation::*;
-mod action;
-pub use action::*;
-mod cube;
-pub use cube::*;
--- rust/src/traits.rs
+++
@@ -1,4 +0,0 @@
-pub trait Indexable<T, S> {
-    fn from_index(index: T) -> Option<S>;
-    fn to_index(&self) -> T;
-}
