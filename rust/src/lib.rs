b5be978fe453fcea5fafb9e7cf427f9d79b0cfbf
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-02-25 10:53:46 -0500

    continued work on rust module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,7 @@
+use std::cell::RefCell;
+
 use cpython::PyResult;
+use sim::Cube;
 
 mod sim;
 
@@ -8,4 +11,12 @@ extern crate cpython;
 py_module_initializer!(librust, |py, m| {
     m.add(py, "__doc__", "This module is implemented in rust")?;
     Ok(())
+});
+
+py_class!(class PyCube |py| {
+    data cube: RefCell<Cube>;
+
+    def __new__(_cls) -> PyResult<PyCube> {
+        PyCube::create_instance(py, RefCell::new(Cube::default()))
+    }
 });
--- rust/src/sim/face.rs
+++ rust/src/sim/face.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Copy)]
 pub enum Face {
     R,
     U,
