523d3fa93e02b0fd4339f6907f73dbcd1e74c82c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-03-02 09:52:26 -0500

    Added python tests for rust module

---
+++ rust/python/test_cube2x2.py
@@ -0,0 +1,24 @@
+import librust
+
+def test_create_cube2x2():
+  cube = librust.PyCube2x2()
+  assert cube is not None
+
+def test_new_cube_is_solved():
+  cube = librust.PyCube2x2()
+  assert cube.is_solved()
+
+def test_apply_valid_action():
+  for i in range(18):
+    cube = librust.PyCube2x2()
+    cube.apply_action(i)
+    assert not cube.is_solved()
+
+def test_get_observation_length():
+  cube = librust.PyCube2x2()
+  assert type(cube.get_observation_length()) == int
+
+def test_get_observations_is_of_length():
+  cube = librust.PyCube2x2()
+  length = cube.get_observation_length()
+  assert len(cube.get_observations()) == length
