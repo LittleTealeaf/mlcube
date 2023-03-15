795134cf970ffb2bc185a3d64b15ed1e12039e85
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-03-15 23:39:57 +0000

    Using pyodbc instead of pymssql

--- model/src/database.py
+++ model/src/database.py
@@ -1,7 +1,37 @@
 from os import getenv
-
-
+from dotenv import load_dotenv
 import pyodbc
+
+load_dotenv()
+
+
+def create_database_connection():
+    server = getenv("SQL_SERVER")
+    port = getenv("SQL_PORT")
+    database = getenv("SQL_DATABASE")
+    username = getenv("SQL_USERNAME")
+    password = getenv("SQL_PASSWORD")
+    driver = "ODBC Driver 17 for SQL Server"
+    return pyodbc.connect(
+        f"DRIVER={driver};SERVER={server},{port};DATABASE={database};UID={username};PWD={password}"
+    )
+
+def get_model_id(name: str, connection=None, create_missing=False):
+    new_connection = not connection
+    if new_connection:
+        connection = create_database_connection()
+
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT ModelId FROM Models WHERE ModelName = \'{name}\'')
+    row = cursor.fetchone()
+    cursor.close()
+
+    if new_connection:
+        connection.commit()
+        connection.close()
+
+    return row[0]
+
 
 
 
