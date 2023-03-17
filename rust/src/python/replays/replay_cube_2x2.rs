56f520563452435102a3efc15e8af40ea72ce0d2
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-03-17 19:40:22 +0000

    added additional functions to replay_cube_2x2

--- rust/src/python/replays/replay_cube_2x2.rs
+++ rust/src/python/replays/replay_cube_2x2.rs
@@ -1,6 +1,9 @@
 use pyo3::{pyclass, pymethods, PyResult};
 
-use crate::{puzzle::puzzles::Cube2x2, replay::Replay};
+use crate::{
+    puzzle::{puzzles::Cube2x2, Puzzle},
+    replay::Replay,
+};
 
 use super::PyReplayEntry;
 
@@ -29,6 +32,22 @@ impl PyReplay2x2 {
             .into_iter()
             .map(PyReplayEntry::from)
             .collect()
+    }
+
+    fn scramble(&mut self, steps: usize) -> u64 {
+        self.replay.scramble(steps)
+    }
+
+    fn scramble_with_seed(&mut self, steps: usize, seed: u64) {
+        self.replay.scramble_with_seed(steps, seed)
+    }
+
+    fn reset(&mut self) {
+        self.replay.reset()
+    }
+
+    fn is_solved(&mut self) -> bool {
+        self.replay.is_solved()
     }
 }
 
