34ad507b14d6d50c82f960296c706ad435ad2c5e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-03-17 17:32:16 +0000

    Added some documentation for Puzzles

--- rust/mlcube.pyi
+++ rust/mlcube.pyi
@@ -1,19 +1,28 @@
 class Puzzle:
     observation_length: int
+    "The length of array that the `get_observations()` method will return. That is, any state output of this puzzle will have a length of this value"
     action_size: int
+    "The number of valid actions that can be performed on the puzzle. Determines the maximum value that can be used in the `apply_action()` method"
     def __init__(self):
+        "Creates a new instance of the puzzle in its solved state"
         ...
     def reset(self):
+        "Resets the puzzle to its solved state"
         ...
     def is_solved(self) -> bool:
+        "Returns `True` if the puzzle is in its solved state, otherwise returns `False`"
         ...
     def get_observations(self) -> list[int]:
+        "Returns an array of observations generated. Each value will either be a 0 or a 1, and the length will be as defined in `observation_length`"
         ...
     def apply_action(self, action: int):
+        "Applies an action to the puzzle, valid action indices are within the range `0 <= {action} < action_size`"
         ...
     def scramble(self, steps: int) -> int:
+        "Scrambles the cube randomly for `steps` steps, and returns the seed used to scramble the cube"
         ...
     def scramble_with_seed(self, steps: int, seed: int):
+        "Scrambles the cube randomly for `steps` steps using the provided seed."
         ...
 
 
