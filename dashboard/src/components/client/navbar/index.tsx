af384fe15665f10d3f9e124bd917ca39e769c573
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-09 13:56:05 -0400

    changes

--- dashboard/src/components/client/navbar/index.tsx
+++ dashboard/src/components/client/navbar/index.tsx
@@ -5,8 +5,8 @@ export default function NavBar({ }) {
 
 
 	return (
-		<div style={{position: 'sticky', top: 0}}>
-				This is the navbar
+		<div style={{ position: 'sticky', top: 0 }}>
+			This is the navbar
 		</div>
 	)
 }
