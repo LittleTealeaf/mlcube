addb26715beea68fd2ee9f854d417b351c88b0c8
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-04-10 12:04:44 -0400

    Reorganize sql scripts

---
+++ sql/tables/epoch.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Epoch (
+    EpochId int identity primary key,
+    ModelId int not null foreign key references Model (ModelId),
+    Epoch int not null,
+    Loss float(53),
+    Reward float(53)
+)
