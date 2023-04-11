351ed39f5da8906ef92a83afe5e20049fdb7b98a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-11 16:19:05 -0400

    rebuilt navbar

--- dashboard/src/components/client/navbar/index.tsx
+++ dashboard/src/components/client/navbar/index.tsx
@@ -1,18 +1,39 @@
 'use client'
 
-import { Button } from "@mui/material"
+import { AppBar, Box, Button, Container, Divider, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
+const navElements = [
+	{
+		name: "Home",
+		path: "/"
+	},
+	{
+		name: "Models",
+		path: "/models"
+	}
+];
 
 export default function NavBar({ }) {
 
-
 	return (
-		<div className="flex p-3">
-			<h2 className="font-bold text-lg grow">
-				MlCube
-			</h2>
-			<Button href="/">Home</Button>
-			<Button href="/models">Models</Button>
-		</div>
+		<AppBar position="static">
+			<Container maxWidth="xl">
+				<Toolbar disableGutters sx={{ display: 'flex' }}>
+					<Typography variant="h6" color="inherit" >
+						MlCube
+					</Typography>
+					<Divider orientation="vertical" flexItem sx={{ m: '10px' }} />
+					{
+						navElements.map(({ name, path }) => (
+							<Button href={path} key={path} color="inherit">
+								{name}
+							</Button>
+						))
+					}
+				</Toolbar>
+			</Container>
+		</AppBar>
 	)
 }
