c1e8db1c6b5654cb054f4e0cccef903316b8f4bf
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-14 12:18:39 -0400

    Added NodeID to PublicNodes.sql

--- sql/views/PublicNodes.sql
+++ sql/views/PublicNodes.sql
@@ -1,7 +1,12 @@
 -- This is a table that is able to be used by the dashboard or publicly facing programs that might want to "simulate"
 -- the neural network
 CREATE VIEW PublicNode AS
-    SELECT
-        ModelId, Layer, X, Y, Weight, Bias
+SELECT ROW_NUMBER() OVER (ORDER BY ModelId, Layer, X, Y) NodeId,
+       ModelId,
+       Layer,
+       X,
+       Y,
+       Weight,
+       Bias
 FROM Node
 WHERE TargetNetwork = 0
