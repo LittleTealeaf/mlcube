2ca38d335649382e8cda2f23ef0347ac4b551d7c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-14 11:59:45 -0400

    changes'

--- python/src/script.py
+++ python/src/script.py
@@ -5,7 +5,7 @@ from mlcube import PyReplay2x2
 
 replay = PyReplay2x2(1000)
 replay.scramble(1000)
-agent = Agent(replay, [400,300,300])
+agent = Agent('testing-123', replay,[400,300,300])
 
 for i in range(10000):
     agent.step_experience( 1 - i / 100000)
