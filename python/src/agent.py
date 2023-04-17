0489a24dd4c4dda4325bb45dc9c4ecc5ef772587
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-04-17 17:09:33 +0000

    Added TODO

--- python/src/agent.py
+++ python/src/agent.py
@@ -112,7 +112,7 @@ class Agent:
 
         while len(steps) < max_steps and not cube.is_solved() and not cube.has_looped():
             network_output = self.network.apply(cube.get_observations())
-            choice = tf.math.argmax(network_output)[0]
+            choice = tf.math.argmax(network_output)[0]  # TODO: This is not getting the right choice!
             cube.apply_action(choice)
             name = cube.get_action_name(choice)
             reward = cube.get_reward()
