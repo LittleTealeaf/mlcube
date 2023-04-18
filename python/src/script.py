45e4ba2dd6347456a1e9f14950e9c1804ba5c880
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-04-18 15:33:48 +0000

    Initial Configuration for Rust-Agent--Test-5

--- python/src/script.py
+++ python/src/script.py
@@ -4,31 +4,41 @@ from database import Database
 import os
 
 
-
-replay = PyReplay2x2(1_000_000)
+replay = PyReplay2x2(10_000)
 
 
 UPDATE_TARGET_INTERVAL = 1000
-EVALUATION_INTERVAL = 250
-SAVE_INTERVAL = 100
+EVALUATION_INTERVAL = 100
+SAVE_INTERVAL = 250
 GAMMA = 0.9
-TRAIN_SAMPLE_SIZE = 2000
+TRAIN_SAMPLE_SIZE = 1000
+EXPERIENCE_GATHER_SIZE = 1000
 
-agent = Agent('Rust-Agent-Test-4', replay,[300,300,300], database=Database())
+
+def calculate_epsilon(epoch):
+    return 1 - ((epoch % UPDATE_TARGET_INTERVAL) / UPDATE_TARGET_INTERVAL)
+
+
+def calculate_learning_rate(epoch):
+    return 0.001 * (0.9 ** (epoch / UPDATE_TARGET_INTERVAL) ) * (0.99 ** ((epoch % UPDATE_TARGET_INTERVAL) / 100))
+
+
+agent = Agent("Rust-Agent-Test-5", replay, [200, 200, 200, 200], database=Database())
 
 while not os.path.exists("./stop"):
     replay.reset()
     replay.scramble()
     epoch = agent.database.get_current_epoch(agent.model_id)
 
-    EPSILON = 1 - ((epoch % UPDATE_TARGET_INTERVAL) / UPDATE_TARGET_INTERVAL)
+    EPSILON = calculate_epsilon(epoch)
+    LEARNING_RATE = calculate_learning_rate(epoch)
 
     print("Epoch ", epoch)
 
-    for _ in range(1000):
+    for _ in range(EXPERIENCE_GATHER_SIZE):
         agent.step_experience(EPSILON)
 
-    agent.train(TRAIN_SAMPLE_SIZE, 0.001 * 0.99 ** (epoch / UPDATE_TARGET_INTERVAL), GAMMA)
+    agent.train(TRAIN_SAMPLE_SIZE, LEARNING_RATE, GAMMA)
 
     if epoch % UPDATE_TARGET_INTERVAL == 0:
         agent.update_target()
