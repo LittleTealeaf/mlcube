be091336c5d41e6fa14f3f14fca0b8859a782e88
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-18 15:59:06 -0400

    restarting

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -6,12 +6,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="overflow-x-hidden flex-col flex h-screen p-0 m-0">
-        <RootNavBar />
-        {children}
-      </body>
-    </html>
-  );
+  return <html lang="en">{children}</html>;
 }
