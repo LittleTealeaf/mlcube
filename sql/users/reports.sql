cc8a69c366e13cbf9972ee631bc0e0f39ada8184
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-20 16:41:38 -0400

    I can connect to the database with R LETS GOOO

---
+++ sql/users/reports.sql
@@ -0,0 +1,9 @@
+CREATE LOGIN Reports WITH PASSWORD = 'mlcube-reporting123'
+
+CREATE USER Reports FOR LOGIN Reports;
+GRANT CONNECT TO Reports;
+
+GRANT SELECT ON Model TO Reports
+GRANT SELECT ON Epoch TO Reports
+GRANT SELECT ON Evaluation TO Reports
+GRANT SELECT ON EvaluationMove TO Reports
