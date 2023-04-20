ef9e0f9493f75e7e01040d2b5a749bcd28a233d9
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-04-20 16:49:05 -0400

    R connection is done with function

--- report/scripts/database.R
+++ report/scripts/database.R
@@ -1,29 +1,39 @@
 dotenv::load_dot_env()
 library(RJDBC)
 
-driver <- JDBC(
-  "com.microsoft.sqlserver.jdbc.SQLServerDriver",
-  Sys.getenv("JDBC_PATH")
-)
+connectToDatabase <- function() {
+  driver <- JDBC(
+    "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+    Sys.getenv("JDBC_PATH")
+  )
 
-conn <- dbConnect(
-  drv = driver,
-  sprintf(
-    "jdbc:sqlserver://%s:%s;databaseName=%s;encrypt=false",
-    Sys.getenv("SQL_HOST"),
-    Sys.getenv("SQL_PORT"),
-    Sys.getenv("SQL_DATABASE")
-  ),
-  Sys.getenv("SQL_USERNAME"),
-  Sys.getenv("SQL_PASSWORD")
-)
+  conn <- dbConnect(
+    drv = driver,
+    sprintf(
+      "jdbc:sqlserver://%s:%s;databaseName=%s;encrypt=false",
+      Sys.getenv("SQL_HOST"),
+      Sys.getenv("SQL_PORT"),
+      Sys.getenv("SQL_DATABASE")
+    ),
+    Sys.getenv("SQL_USERNAME"),
+    Sys.getenv("SQL_PASSWORD")
+  )
 
-rs <- dbSendQuery(conn, "SELECT * FROM Epoch WHERE ModelId = 2043")
-df <- dbFetch(rs, n = -1)
-View(df)
-
-
+  return(conn)
+}
 
 
-dbDisconnect(conn)
+# dotenv::load_dot_env()
+# library(RJDBC)
+#
 
+#
+# rs <- dbSendQuery(conn, "SELECT * FROM Epoch WHERE ModelId = 2043")
+# df <- dbFetch(rs, n = -1)
+# View(df)
+#
+#
+#
+#
+# dbDisconnect(conn)
+#
