044680922ffeb788dab24e6b5802249a0887262e
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2023-05-02 11:30:38 -0400

    Update Report Analysis

--- report/scripts/report_analysis.R
+++ report/scripts/report_analysis.R
@@ -6,12 +6,16 @@ source("src/database.R")
 
 df_epochs <- rbind(
   get_epochs(2043) %>% mutate(Loss = sqrt(Loss)),
-  get_epochs(2044,2045,2046,2048)
+  get_epochs(2044, 2045, 2046, 2048)
 ) %>% mutate(Model = factor(ModelId))
 
 df_epochs %>%
   filter(Epoch <= 5000 & Loss < 8) %>%
-  ggplot(aes(x = Epoch, y = Loss, color=Model)) +
+  ggplot(aes(x = Epoch, y = Loss, color = Model)) +
   geom_point(size = 0.1) +
-  facet_wrap(~Model, ncol=1) +
+  facet_wrap(~Model, ncol = 1) +
   theme(legend.position = "none")
+
+
+get_epochs(2051) %>% ggplot(aes(x = Epoch, y = Loss)) +
+  geom_point()
