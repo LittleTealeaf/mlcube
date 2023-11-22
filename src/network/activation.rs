04445951a9a9ac4bb517eb0376c14e2f90b9bbb1
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-11-22 12:12:37 -0500

    changes

--- src/network/activation.rs
+++ src/network/activation.rs
@@ -1,4 +1,4 @@
-use crate::utils::Sigmoid;
+use crate::utils::Relu;
 
 pub trait Activation {
     fn activation(self) -> Self;
@@ -7,10 +7,10 @@ pub trait Activation {
 
 impl Activation for f64 {
     fn activation(self) -> Self {
-        self.sigmoid()
+        self.relu()
     }
 
     fn activation_derivative(self) -> Self {
-        self.sigmoid_derivative()
+        self.relu_derivative()
     }
 }
