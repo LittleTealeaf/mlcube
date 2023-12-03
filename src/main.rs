45c79b37fff408fc58c55425da80e054551b9ec8
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-03 13:55:36 -0500

    change to generous eight

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use std::fs;
 use mlcube::{
     agent::{Agent, FnValue, NewAgentConfig, SampleStrategy},
     network::SolveResult,
-    puzzle::{environments::EightPuzzle, Puzzle},
+    puzzle::{environments::{EightPuzzle, GenerousEight}, Puzzle},
 };
 use rand::{seq::SliceRandom, thread_rng};
 
 const EVALUATE_INTERVAL: usize = 100;
 
-type _Puzzle = EightPuzzle;
+type _Puzzle = GenerousEight;
 
 fn main() {
     let mut agent: Agent<_Puzzle> = Agent::new(NewAgentConfig {
