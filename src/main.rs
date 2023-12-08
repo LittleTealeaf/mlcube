2f1e69dce288d1d76f93d6b6befed03f9e00700a
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-08 12:54:19 -0500

    Increase threshold for changing target

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn main() {
         update_strategy: UpdateStrategy::Threshold {
             initial_update_epoch: 100,
             test_size: 100,
-            threshold: 0.0001,
+            threshold: 0.001,
             minimum_update_interval: 100,
         },
         max_replay_size: 100_000,
