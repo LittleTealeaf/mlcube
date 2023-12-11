9d451b0f2844a207587334872123a844a49a497c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-11 11:45:46 -0500

    reduce max update

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
             test_size: 100,
             initial_update: Some(100),
             min_update: Some(100),
-            max_update: Some(10_000),
+            max_update: Some(1_000),
             threshold: 0.01,
         },
         max_replay_size: 1_000_000,
