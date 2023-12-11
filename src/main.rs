ce370d752d2711b61a4f617bb58765b50c9ec2a7
Thomas Kwashnak <thomaskwashnak@gmail.com>
2023-12-11 08:52:17 -0500

    3x3 lights out

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@ use rand::{seq::SliceRandom, thread_rng};
 
 const EVALUATE_INTERVAL: usize = 20;
 
-type _Puzzle = LightsOut<2, 2>;
+type _Puzzle = LightsOut<3, 3>;
 
 fn main() {
     let mut agent: Agent<_Puzzle> = Agent::new(NewAgentConfig {
-        hidden_layers: vec![10; 3],
+        hidden_layers: vec![50; 3],
         gamma: 0.9,
         alpha: FnValue::from(0.1)
             * FnValue::from(0.99).exp((FnValue::Epoch - FnValue::LastTargetUpdate) + 1.0.into()),
